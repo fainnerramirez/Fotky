ac6973a80ad7bcf3eaa0510053763bdf72169756
fainnerramirezweb <FainnerRamirez-Web>
2021-05-24 20:47:21 -0500

    corregidos los errores al hacer la peticios a la Api

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -4,60 +4,42 @@ import dotenv from "dotenv";
 //Css Components
 import HomeWrapp from "./styles/stylesHome/home";
 //Bootstrap 4
-import Card from "react-bootstrap/Card";
+import Button from "react-bootstrap/Button";
 //config dotenv
 dotenv.config();
 //const
 const CLIENT_API = process.env.REACT_APP_UNPLASH_KEY;
 
-/*
-type Props = {
-  url: string;
-  alt?: string;
-};
-*/
-const fetchApi: Function = async (SEARCH_USER: string) => {
-  return await axios
+const fetchApi = (searchUser: string, root: HTMLElement) => {
+  return axios
     .get(
-      `https://api.unsplash.com/search/photos?page=1&query=${SEARCH_USER}&&client_id=${CLIENT_API}`
+      `https://api.unsplash.com/search/photos?page=1&query=${searchUser}&&client_id=${CLIENT_API}`
     )
-    .then(function (res: any) {
-      const cardColumn = document.getElementById("card-column");
-      res.data.results.map((item: any) => {
-        cardColumn
-          ? (cardColumn.innerHTML += `
-        <div class="card" style="width: 300px">
-        <img src=${item.urls.regular} class="card-img-top" alt="image api" style="width: 100%; height="250px">
-        <div class="card-body">
-          <h5 class="card-title">Card title</h5>
-          <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-          <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
-        </div>
-      </div>
-          `)
-          : console.log("Error");
+    .then(({ data }) => {
+      const { results } = data;
+
+      results.map((item: any) => {
+        root.innerHTML += `
+         <div class="card" style="width: 300px">
+           <img src=${item.urls.regular} class="card-img-top" alt="image api" style="width: 100%; height="250px">
+           <div class="card-body">
+             <h5 class="card-title">Card title</h5>
+             <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+             <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+           </div>
+         </div>`;
       });
     })
-    .catch(function (error: any) {
-      console.log(error);
-    });
-};
-
-const getImages: Function = (inputUser: string) => {
-  const inputSearch = document.getElementById("input-search");
-  inputSearch?.addEventListener("keydown", (e) => {
-    console.log(e.key);
-    if (e.key === "Enter") {
-      e.preventDefault();
-      fetchApi(inputUser);
-    } else {
-      console.log("error al cargar la img");
-    }
-  });
+    .catch(console.error);
 };
 
 const Home: React.FC = () => {
   const [images, setImages] = useState("");
+
+  useEffect(() => {
+    const root = document.getElementById("root-images") || null;
+    root ? fetchApi(images, root) : console.log("root es null: ", root);
+  });
 
   return (
     <HomeWrapp>
@@ -72,11 +54,10 @@ const Home: React.FC = () => {
             id="input-search"
             onChange={(e) => setImages(e.target.value)}
           />
+          <Button className="ml-4">Buscar</Button>
         </div>
       </div>
-      <div id="card-column" className="card-column">
-        {getImages(images)}
-      </div>
+      <div id="root-images"></div>
     </HomeWrapp>
   );
 };
